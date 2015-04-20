3cf84a3573cfe3bfc050665ed2c974bdddb50357
Tetratrio <adrian.sahlman@gmail.com>
2015-04-20 16:41:18 +0200

    sql

---
+++ init.sql
@@ -0,0 +1,28 @@
+set foreign_key_checks = 0;
+
+drop table if exists User;
+drop table if exists Bike;
+
+CREATE TABLE User (
+  username CHAR(10),
+  info TEXT,
+  password CHAR(4),
+  checkedIn TIMESTAMP,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE Bike (
+  bikeId INT(13) AUTO_INCREMENT,
+  username CHAR(10),
+    PRIMARY KEY (bikeId),
+    FOREIGN KEY (username) REFERENCES User (username)
+);
+
+insert into User (username, password)
+            values ("9003241974", "1337");
+
+insert into Bike (username)
+            values ("9003241974");
+
+
+set foreign_key_checks = 1;
