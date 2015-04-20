ea4717b722ff8e3f8bdd966c0cded0da81505526
Tetratrio <adrian.sahlman@gmail.com>
2015-04-20 17:17:51 +0200

    sql update

---
+++ sql/insert_test_values.sql
@@ -0,0 +1,48 @@
+/* Insert new users */
+insert into User (username, password)
+            values ("8603241418", "1337");
+
+insert into User (username, password)
+            values ("9109258474", "1234");
+
+insert into User (username, password)
+            values ("9010101111", "1111");
+
+insert into User (username, password)
+            values ("6707051020", "8877");
+
+insert into User (username, password)
+            values ("9305222074", "5565");
+
+/* Insert new bikes */
+insert into Bike (username)
+            values ("8603241418");
+
+insert into Bike (username)
+            values ("8603241418");
+
+insert into Bike (username)
+            values ("8603241418");
+
+insert into Bike (username)
+            values ("8603241418");
+
+insert into Bike (username)
+            values ("9109258474");
+
+insert into Bike (username)
+            values ("9109258474");
+
+insert into Bike (username)
+            values ("9109258474");
+
+insert into Bike (username)
+            values ("9010101111");
+
+insert into Bike (username)
+            values ("9010101111");
+
+insert into Bike (username)
+            values ("6707051020");
+
+
